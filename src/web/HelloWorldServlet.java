126f431e520f6669bb0e0e9530ec6d110b0be910
AM <amarcovich@inndatsys.com>
2019-08-22 11:54:57 -0500

    Commit from Eclipse

--- src/web/HelloWorldServlet.java
+++ src/web/HelloWorldServlet.java
@@ -30,7 +30,7 @@ public class HelloWorldServlet extends HttpServlet {
 	{
 		JSONObject obj = new JSONObject();
 		
-		obj.put("name", "jenkins");
+		obj.put("name", "alm");
 		obj.put("num", new Integer(100));
 		obj.put("balance", new Double(1000.21));
 		obj.put("is_vip", new Boolean(true));
