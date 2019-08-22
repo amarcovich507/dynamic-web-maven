3cd37d86a766798460b26e82a6889158334fed95
AM <amarcovich@inndatsys.com>
2019-08-21 19:56:03 -0500

    first commit

---
+++ src/web/HelloWorldServlet.java
@@ -0,0 +1,49 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/LoginServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		JSONObject obj = new JSONObject();
+		
+		obj.put("name", "foo");
+		obj.put("num", new Integer(100));
+		obj.put("balance", new Double(1000.21));
+		obj.put("is_vip", new Boolean(true));
+		
+		response.getWriter().append("Served GJson: ").append(obj.toJSONString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
